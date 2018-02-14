b3cfceaf7f19a4b54cd40bb3083a8edadc745f08
Julian Ćwirko <julian.cwirko@gmail.com>
2018-02-14 20:49:42 +0100

    updates

--- package.js
+++ package.js
@@ -1,30 +1,30 @@
 Package.describe({
-    summary: 'Minifier for Meteor with PostCSS processing - use Autoprefixer and others with ease',
-    version: '1.2.0',
-    name: 'juliancwirko:postcss',
-    git: 'https://github.com/juliancwirko/meteor-postcss.git'
+  summary: 'Minifier for Meteor with PostCSS processing - use Autoprefixer and others with ease',
+  version: '1.3.0',
+  name: 'juliancwirko:postcss',
+  git: 'https://github.com/juliancwirko/meteor-postcss.git'
 });
 
 Package.registerBuildPlugin({
-    name: 'minifier-postcss',
-    use: [
-        'ecmascript@0.5.9',
-        'minifier-css@1.2.15'
-    ],
-    npmDependencies: {
-        'source-map': '0.5.6',
-        'postcss': '5.2.5',
-        'app-module-path': '2.1.0'
-    },
-    sources: [
-        'plugin/minify-css.js'
-    ]
+  name: 'minifier-postcss',
+  use: [
+    'ecmascript@0.10.4',
+    'minifier-css@1.3.1'
+  ],
+  npmDependencies: {
+    'source-map': '0.5.6',
+    'postcss': '6.0.17',
+    'app-module-path': '2.2.0'
+  },
+  sources: [
+    'plugin/minify-css.js'
+  ]
 });
 
-Package.onUse(function(api) {
-    api.use('isobuild:minifier-plugin@1.0.0');
+Package.onUse(function (api) {
+  api.use('isobuild:minifier-plugin@1.0.0');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
 
 });
